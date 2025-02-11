9b3ecfe4c6462d2bc1504d368428cdbc95ed5dee
Alphis <alphis@MacBook-Air-Alphis.local>
2025-02-11 22:13:16 +0300

    SAVE

--- src/pageComponents/MainPage/MainPage.tsx
+++ src/pageComponents/MainPage/MainPage.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import StartBannerSection from '@/sections/StartBannerSection/StartBannerSection';
+import WelcomZordSection from '@/sections/WelcomZordSection/WelcomZordSection';
+import React from 'react';
+import styles from './MainPage.module.scss';
+import SliderSection from '@/sections/SliderSection/SliderSection';
+import EveryoneSection from '@/sections/EveryoneSection/EveryoneSection';
 
 export const MainPage = () => {
   return (
-    <main>MainPage</main>
-  )
-}
+    <main className={styles.root}>
+      <StartBannerSection />
+      <WelcomZordSection />
+      <SliderSection />
+      <EveryoneSection />
+    </main>
+  );
+};
---
+++ src/sections/StartBannerSection/StartBannerSection.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './StartBannerSection.module.scss';
+
+const StartBannerSection = () => {
+  return <section className={styles.banner}></section>;
+};
+
+export default StartBannerSection;
